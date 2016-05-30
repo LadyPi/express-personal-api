624f239a5d0fd8ef218eb39f633048342faf1188
LadyPi <ms.vanessa.farias.dm@gmail.com>
2016-05-30 11:24:03 -0700

    updated paths

---
+++ models/destinations.js
@@ -0,0 +1,17 @@
+//importing mongoose
+var mongoose = require('mongoose'),
+//using mongoose to create schema
+  Schema = mongoose.Schema;
+
+//profile schema creation format
+var DestinationSchema = new Schema({
+  location: String,
+  country: String,
+});
+
+//profile schema model creation
+var Destination = mongoose.model('Destination', DestinationSchema);
+
+//exporting profile model to be used outside of this file
+module.exports = Destination;
+
--- models/index.js
+++ models/index.js
@@ -1,13 +1,7 @@
 var mongoose = require("mongoose");
-mongoose.connect( process.env.MONGOLAB_URI ||
-                  process.env.MONGOHQ_URL ||
+mongoose.connect( process.env.MONGOLAB_URI || 3000,
+                  process.env.MONGOHQ_URL || 3000,
                   "mongodb://localhost/personal-api");
 
 
-
-
-
-
-
-
-module.exports.Profile = require("./profile.js");
+module.exports.Profile = require("./destinations.js");
