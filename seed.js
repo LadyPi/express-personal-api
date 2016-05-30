006e1b9ed0f493eaff734700a12a1f6e8ef7562b
LadyPi <ms.vanessa.farias.dm@gmail.com>
2016-05-30 09:25:27 -0700

    mapping endpoints

--- seed.js
+++ seed.js
@@ -1,15 +1,39 @@
 // This file allows us to seed our application with data
 // simply run: `node seed.js` from the root of this project folder.
 
-// var db = require('./models');
+var db = require('./models');
 
-// var new_campsite = {description: "Sharp rocks. Middle of nowhere."}
+var destinationWishList = [
+				{
+				 destination: 'Amsterdam',
+	             country: 'The Netherlands'
+				},
+				{
+				 destination: 'Venice',
+	             country: 'Italy'
+				},
+				{
+				 destination: 'Madrid',
+	             country: 'Spain'
+				}
+];
 
-// db.Campsite.create(new_campsite, function(err, campsite){
-//   if (err){
-//     return console.log("Error:", err);
-//   }
 
-//   console.log("Created new campsite", campsite._id)
-//   process.exit(); // we're all done! Exit the program.
-// })
+db.Destination.create(destinationWishList, function(err, destinations) {
+  if (err){
+    return console.log("Error:", err);
+  }
+  });
+
+  console.log("Created new destination", destination._id);
+  process.exit(); // we're all done! Exit the program.
+
+
+
+
+
+
+// At least one resource (mongoose model) that you can CRUD using RESTful Routes
+// That means endpoints for index, show, create update, delete!
+
+// All API Endpoints must return JSON.
--- server.js
+++ server.js
@@ -6,12 +6,14 @@ var express = require('express'),
 // and populate the req.body object
 var bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({ extended: true }));
+//app.use(bodyParser.json());
+
 
 /************
  * DATABASE *
  ************/
 
-// var db = require('./models');
+var db = require('./models');
 
 /**********
  * ROUTES *
@@ -37,18 +39,58 @@ app.get('/', function homepage(req, res) {
 app.get('/api', function api_index(req, res) {
   // TODO: Document all your api endpoints below
   res.json({
-    // woops_i_has_forgot_to_document_all_my_endpoints: true, // CHANGE ME ;)
     message: "Welcome to my personal api! Here's what you need to know!",
-    documentation_url: "https://github.com/example-username/express_self_api/README.md", // CHANGE ME
-    base_url: "http://YOUR-APP-NAME.herokuapp.com", // CHANGE ME
+    documentation_url: "https://github.com/LadyPi/express-personal-api", // CHANGE ME, changed url
+    base_url: "https://agile-escarpment-42560.herokuapp.com",
     endpoints: [
-      {method: "GET", path: "/api", description: "Describes all available endpoints"},
-      {method: "GET", path: "/api/profile", description: "Data about me"}, // CHANGE ME
-      {method: "POST", path: "/api/campsites", description: "E.g. Create a new campsite"} // CHANGE ME
+      {method: "GET", path: "/api", description: "Describes all available endpoints."},
     ]
   });
 });
+///////////////////////
+// {method: "GET", path: "/api/destinations", description: "Vaca destination wishlist."}
 
+app.get('/api/destinations', function destinationWishList_Index(req, res) {
+  db.Destination.find({}, function(err, destinations) {
+    res.json(destinations);
+  });
+});
+
+///////////////////////
+// {method: "POST", path: "/api/destinations", description: "Create a destination."}
+
+app.post('/api/destinations', function destinationWishList_Create(req, res) {
+  console.log('body', req.body);
+
+///////////////////////
+
+db.Destination.create(req.body, function(err, album) {
+    if (err) { console.log('error', err); }
+    console.log(destination);
+    res.json(destination);
+ });
+
+///////////////////////
+
+
+
+       // CHANGE ME
+      {method: "POST", path: "/api/profiles", description: "Create a profile."}, // CHANGE ME
+      {method: "GET", path: "/api/profile_id", description: "Get a single profile."}, // CHANGE ME
+      {method: "PUT", path: "/api/:profile_id", description: "Update a profile with new info."}, // CHANGE ME
+      {method: "DELETE", path: "/api/:profile_id", description: "Delete a profile."}, // CHANGE ME
+      
+
+      // {method: "GET", path: "/api", description: "Describes all available endpoints."},
+      // DOES IT MATTER THAT THIS IS SEED.JS?
+      {method: "GET", path: "/api/vacaWishList", description: "My Vaca Wishlist."}, // CHANGE ME
+      // {method: "POST", path: "/api/vacaWishList", description: "Create destination."}, // CHANGE ME
+      {method: "GET", path: "/api/vacaWishList_id", description: "Get a single destination."}, // CHANGE ME
+      {method: "PUT", path: "/api/:vacaWishList_id", description: "Update a destination with new info."}, // CHANGE ME
+      {method: "DELETE", path: "/api/:vacaWishList_id", description: "Delete a destination."}, // CHANGE ME
+    ]
+  });
+});
 /**********
  * SERVER *
  **********/
